71d18de5388b2c4ec8d99d254fd55189d6f6b609
unknown <audtracy@outlook.com>
2024-10-23 09:57:32 -0500

    comment code

--- TCPClient.java
+++ TCPClient.java
@@ -5,32 +5,32 @@ import java.net.*;
 public class TCPClient {
 
     public static void main(String argv[]) throws Exception {
-        String sentence = "";
-        String reply;
+        String message = ""; // message client is sending to server
+        String reply; // reply from server to client
 
         int port = 6789;
 
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in)); // read client messages
 
-        Socket clientSocket = new Socket("localhost", port); //localhost = 127.0.0.1
+        Socket clientSocket = new Socket("localhost", port); // localhost = 127.0.0.1
 
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream()); // send messages to server
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // get messages from server
 
         System.out.println("The TCP client is on. Please enter your input:");
 
-        sentence = inFromUser.readLine();
-        while (!sentence.equals("exit")) {
+        message = userInput.readLine(); // read message from user
+        while (!message.equals("exit")) { // end client if user types "exit"
 
-            outToServer.writeBytes(sentence + '\n');
+            out.writeBytes(message + '\n'); // send user message to server
 
-            reply = inFromServer.readLine();
+            reply = in.readLine(); // read response from server
 
-            System.out.println("FROM SERVER: " + reply);
-            sentence = inFromUser.readLine();
+            System.out.println("FROM SERVER: " + reply); // print response from server
+            message = userInput.readLine(); // read new message from user
         }
-        outToServer.writeBytes(sentence + '\n');
-        Thread.sleep(2000); // waits for 2 seconds
+        out.writeBytes(message + '\n');
+        Thread.sleep(2000); // wait for 2 seconds
         clientSocket.close();
 
     }
--- TCPServer.java
+++ TCPServer.java
@@ -10,16 +10,16 @@ public class TCPServer {
 
         int port = 6789;
 
-        ServerSocket welcomeSocket = new ServerSocket(port);
+        ServerSocket welcomeSocket = new ServerSocket(port); // listen for connections
         System.out.println("TCP server is listening on port " + port + "\n");
 
         while (true) {
 
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("[" + connectionSocket.getInetAddress().toString().substring(1) + ":" + connectionSocket.getPort() + "] CONNECTED");
+            Socket connectionSocket = welcomeSocket.accept(); // accept new connection
+            System.out.println("[" + connectionSocket.getInetAddress().toString().substring(1) + ":" + connectionSocket.getPort() + "] CONNECTED"); // print user IP & port address
 
             Runnable clientHandler = new ClientThread(connectionSocket);
-            Thread thread = new Thread(clientHandler);
+            Thread thread = new Thread(clientHandler); // start new thread for client
             thread.start();
 
         }
@@ -27,41 +27,41 @@ public class TCPServer {
     }
 
     public static synchronized int messageIncrement() {
-        return ++totalMessages;
+        return ++totalMessages; // increment shared resource totalMessages within synchronized method to handle race conditions
     }
     
     
     static class ClientThread implements Runnable {
 
         private Socket clientSocket;
-        private int messageCount = 0;
+        private int messageCount = 0; // number of messages sent to this client
 
         public ClientThread(Socket socket) {
-            this.clientSocket = socket;
+            this.clientSocket = socket; 
         }
 
         @Override
         public void run() {
 
-            String clientID = "[" + clientSocket.getInetAddress().toString().substring(1) + ":" + clientSocket.getPort() + "]";
+            String clientID = "[" + clientSocket.getInetAddress().toString().substring(1) + ":" + clientSocket.getPort() + "]"; // IP and port number
 
             try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                String message;
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  // get messages from client
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // send messages to client
+                String message; 
                 while ((message = in.readLine()) != null) {
-                    if (message.equalsIgnoreCase("exit")) {
+                    if (message.equalsIgnoreCase("exit")) { // end client if exit is typed (ends loop and closes socket)
                         System.out.println(clientID + " DISCONNECTED");
                         break;
                     }
 
-                    int totalMessages = TCPServer.messageIncrement();
+                    int totalMessages = TCPServer.messageIncrement(); // increment shared resource totalMessages
 
-                    System.out.println(clientID + " MESSAGE: " + message);
+                    System.out.println(clientID + " MESSAGE: " + message); // print client IP and port number, message, and number of messages
                     System.out.println("\tFrom User: " + ++messageCount);
                     System.out.println("\tTotal Messages: " + totalMessages);
 
-                    out.println("Total Messages: " + totalMessages);
+                    out.println("Total Messages: " + totalMessages); // send message total to client
                 }
                 clientSocket.close();
             }
